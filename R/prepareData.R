18151711ba8b40d416101469b4d83740420e1253
Neda Daneshgar <ne_da978@stu-mail.um.ac.ir>
2017-04-06 05:20:58 +0000

    version 1.0.6

--- R/prepareData.R
+++ R/prepareData.R
@@ -30,7 +30,7 @@ if (ul_t) aa[,2] = culf (aa [,2], lower = all)
 rm (s_sen, t_sen)
 gc ()
 
-aa = tokens(aa, removePunct = removePt)
+aa = tokenize(aa, removePunct = removePt)
 len2 = length(aa) / 2
 
 aa4 = aa [1 : len2]
