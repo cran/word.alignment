c38ff4bc0cb5b73aa717d2b4da98b9c4f83837fd
Neda Daneshgar <ne_da978@stu-mail.um.ac.ir>
2018-01-24 11:24:56 +0000

    version 1.0.8

--- R/prepareData.R
+++ R/prepareData.R
@@ -30,24 +30,13 @@ if (ul_t) aa[,2] = culf (aa [,2], lower = all)
 rm (s_sen, t_sen)
 gc ()
 
-aa = tokenize(aa, remove_punct = removePt)
+aa = tokens(aa, remove_punct = removePt)
 len2 = length(aa) / 2
 
-aa4 = aa [1 : len2]
-aa5 = aa [ (len2+1) : (2 * len2)]
+word2 = aa [1 : len2]
+word3 = aa [ (len2+1) : (2 * len2)]
 
-aa= cbind (aa4, aa5)
-aa = aa [apply(aa, 1, function(x) length (x[1] $ aa4) * length (x [2] $ aa5) != 0)]
-len2 = length(aa) / 2
-
-word4 = cbind (word2 = aa[1:len2], word3 = aa[ (len2 + 1) : (2 * len2)])
-word4 = word4 [apply (word4, 1, function(x) length(x[1] $ word2) * length (x[2] $ word3) != 0)]
- 
-word2 = word4[1 : (length(word4) / 2)]
-word3 = word4[(length(word4) / 2 + 1) : length(word4)]  
- 
-aa = cbind( sapply(word2 , paste, collapse = ' '), sapply(word3, paste, collapse = ' '))
-
+aa = cbind (sapply(word2,paste, collapse = ' '), sapply(word3, paste, collapse = ' '))
 aa = aa [apply (aa, 1, function(x) prod (vapply (strsplit (x, ' '), length, FUN.VALUE=0) >= minlen)& prod (vapply (strsplit (x, ' '), length, FUN.VALUE=0) <= maxlen) == 1) ,]
 
 if(word_align) 
